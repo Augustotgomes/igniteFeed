037bd359dae5f38b99c16e3a6275d90b740ecb6b
Augusto Gomes <augusto.tgomes@gmail.com>
2022-11-10 17:18:58 -0300

    componente comentários e estilização do mesmo

---
+++ src/componets/Comment.jsx
@@ -0,0 +1,31 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/60794553?v=4" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Augusto Gomes</strong>
+              <time title="09 de Novembro as 17:33" dateTime="2022-11-09">cerca de 1h atrás</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
